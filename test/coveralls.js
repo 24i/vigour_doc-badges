384b03bcb867e54db866163b7fbc8bfcf20ba095
Danillo Corvalan <danillo.corvalan@gmail.com>
2016-03-30 17:33:15 +0200

    warning for coveralls  #4

--- test/coveralls.js
+++ test/coveralls.js
@@ -2,6 +2,7 @@
 
 var test = require('tape')
 var badges = require('../')
+var pkgJson = require('../package.json')
 
 var expected = '[![Coverage Status](https://coveralls.io/repos/github/vigour-io/doc-badges/badge.svg?branch=master)](https://coveralls.io/github/vigour-io/doc-badges?branch=master)'
 
@@ -19,4 +20,9 @@ test('coveralls', function (t) {
       'badges(' + JSON.stringify(testCases[i][0]) + ') === ' + testCases[i][1]
     )
   }
+})
+
+test('should have docs property in packagejson', function (t) {
+  t.plan(1)
+  t.ok(pkgJson.docs)
 })
